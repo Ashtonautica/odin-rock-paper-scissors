aeea7ea8f1d7347f66fdc7a03f720e84d371991c
Ashtonaut <ashtonautica@gmail.com>
2023-08-07 18:24:16 +0100

    Add playRound function

--- script.js
+++ script.js
@@ -12,3 +12,34 @@ function getComputerChoice() {
             break;
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    let formattedPlayerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    if (formattedPlayerSelection == computerSelection) {
+        return `A draw! Both sides chose ${formattedPlayerSelection}`;
+    }
+    if (formattedPlayerSelection == "Rock") {
+        if (computerSelection == "Paper") {
+            return "You lose! Paper beats Rock"
+        }
+        else if (computerSelection == "Scissors") {
+            return "You win! Rock beats Scissors"
+        }
+    }
+    if (formattedPlayerSelection == "Paper") {
+        if (computerSelection == "Rock") {
+            return "You win! Paper beats Rock"
+        }
+        else if (computerSelection == "Scissors") {
+            return "You lose! Scissors beats Paper"
+        }
+    }
+    if (formattedPlayerSelection == "Scissors") {
+        if (computerSelection == "Rock") {
+            return "You lose! Rock beats Scissors"
+        }
+        else if (computerSelection == "Paper") {
+            return "You win! Scissors beats Paper"
+        }
+    }
+}
